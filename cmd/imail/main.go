b66171fb7297224ee2a52087922d95b4611f4545
fganga <fganga@unaj.edu.ar>
2023-12-07 11:25:44 -0300

    add command to setup account.

--- cmd/imail/main.go
+++ cmd/imail/main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"syscall"
 
 	"os/exec"
 
 	"github.com/emersion/go-imap/client"
+	"github.com/zalando/go-keyring"
+	"golang.org/x/term"
 )
 
+func usage() {
+	fmt.Println(`usage: imail <command> [parameters]
+    comands: setup, run`)
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "setup":
+		setup()
+		return
+	case "run":
+		run()
+	}
+
+}
+
+func setup() {
+
+	fmt.Println("Enter mail: ")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	mail := scanner.Text()
+
+	fmt.Print("Password: ")
+	bytepw, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		os.Exit(1)
+	}
+	pass := string(bytepw)
+
+	err = keyring.Set("goimail", mail, pass)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() {
+
+	// get password
+	secret, err := keyring.Get("goimail", "fmanuelganga@gmail.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// TODO: this probably could be configurable for now only support gmail
 	mc, err := client.DialTLS("imap.gmail.com:993", nil)
 
@@ -18,7 +71,7 @@ func main() {
 		panic(err)
 	}
 
-	err = mc.Login("fmanuelganga@gmail.com", os.Getenv("PASS"))
+	err = mc.Login("fmanuelganga@gmail.com", secret)
 
 	if err != nil {
 		panic(err)
