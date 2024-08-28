228d69ed77dd2bf22a2126dfc38417c402609877
fganga <fganga@unaj.edu.ar>
2024-08-28 20:27:51 -0300

    merge no-subjects.

--- cmd/imail/main.go
+++ cmd/imail/main.go
@@ -1,38 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"syscall"
+	"time"
+
 	"os/exec"
+	"os/signal"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
+	"github.com/gookit/goutil/dump"
+	"github.com/zalando/go-keyring"
+	"golang.org/x/term"
 )
 
-var lastUid uint32
+func usage() {
+	fmt.Println(`usage: imail <command> [parameters]
+    comands: setup, run`)
+}
 
 func main() {
-	// TODO: this probably could be configurable for now only support gmail
-	mc, err := client.Dial("localhost:143")
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "setup":
+		setup()
+		return
+	case "run":
+		run()
+	}
+
+}
+
+func setup() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter mail: ")
+	scanner.Scan()
+
+	mail := scanner.Text()
+
+	fmt.Print("Password: ")
+	bytepw, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		os.Exit(1)
+	}
+	pass := string(bytepw)
+
+	err = keyring.Set("goimail", mail, pass)
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+}
 
-	err = mc.Login("ubuntu", "asdfqwer")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer mc.Logout()
-
-	if _, err := mc.Select("INBOX", false); err != nil {
-		log.Fatal(fmt.Errorf("select INBOX error: %v", err))
-	}
-
+func process(mc *client.Client) {
 	// Create a channel to receive mailbox updates
-	updates := make(chan client.Update, 0)
+	updates := make(chan client.Update)
 	mc.Updates = updates
 
 	for {
@@ -46,7 +77,6 @@ func main() {
 		// Listen for updates
 
 		exit := false
-
 		last_updates := make(chan client.MailboxUpdate, 1)
 
 		for up := range updates {
@@ -56,7 +86,6 @@ func main() {
 			switch update := up.(type) {
 			case *client.MailboxUpdate:
 				last_updates <- *update
-
 				exit = true
 			}
 		}
@@ -64,18 +93,111 @@ func main() {
 		close(stop)
 
 		if err := <-doneIdle; err != nil {
-			log.Fatal(fmt.Errorf("idle error: %v", err))
+			log.Fatal(err)
 		}
 
 		fmt.Println("Done idling....")
 
 		update := <-last_updates
-		fetchMessageSubject(mc, update)
+
+		fetchMessageAndNotify(mc, update)
+	}
+}
+
+func makeTestClient() *client.Client {
+	mc, err := client.Dial("localhost:143")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = mc.Login("ubuntu", "asdfqwer")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return mc
+}
+
+func makeClient() *client.Client {
+	// get password
+	secret, err := keyring.Get("goimail", "fmanuelganga@gmail.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: this probably could be configurable for now only support gmail
+	mc, err := client.DialTLS("imap.gmail.com:993", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: dont have this hardcoded
+	err = mc.Login("fmanuelganga@gmail.com", secret)
+
+	if err != nil {
+		log.Fatalf("Login Error: %s", err.Error())
+	}
+
+	return mc
+}
+
+func run() {
+
+	mc := makeClient()
+
+	defer mc.Logout()
+
+	_, err := mc.Select("INBOX", true)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: config flag for this
+	ticker := time.NewTicker(10 * time.Minute)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				messages := fetchUnseenMessages(mc)
+				notifyMessages(messages)
+			}
+		}
+	}()
+
+	exitSignal := make(chan os.Signal)
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+	<-exitSignal
+
+	ticker.Stop()
+	done <- true
+	fmt.Println("Application Stopped")
+
+}
+
+func notifyMessages(messages []*imap.Message) {
+
+	header := fmt.Sprintf("Unread Mail (%d)", len(messages))
+
+	msg := messages[len(messages)-1]
+
+	body := fmt.Sprintf("[%s]\\n%s", msg.Envelope.From[0].Address(), msg.Envelope.Subject)
+
+	cmd := exec.Command("notify-send", header, body, "--icon=mail-unread")
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
 }
 
-func fetchMessageSubject(mc *client.Client, update client.MailboxUpdate) {
+func fetchMessageAndNotify(mc *client.Client, update client.MailboxUpdate) {
 	messages := make(chan *imap.Message, 1)
 	doneFetch := make(chan error, 1)
 
@@ -105,5 +227,79 @@ func fetchMessageSubject(mc *client.Client, update client.MailboxUpdate) {
 	if err := <-doneFetch; err != nil {
 		log.Fatal(err)
 	}
+}
+
+func fetchMessage(mc *client.Client, uid uint32) {
+	messages := make(chan *imap.Message, 1)
+	doneFetch := make(chan error, 1)
+
+	seqset := new(imap.SeqSet)
+	seqset.AddNum(uid)
+
+	go func() {
+		doneFetch <- mc.UidFetch(seqset, []imap.FetchItem{imap.FetchBody, imap.FetchEnvelope}, messages)
+	}()
+
+	msg := <-messages
+	if msg != nil {
+		dump.P(msg)
+	} else {
+		log.Fatal("msg is nil")
+	}
+
+	if err := <-doneFetch; err != nil {
+		log.Fatal(err)
+	}
+}
+
+func listMailboxes(mc *client.Client) {
+	mailboxes := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+
+	go func() {
+		done <- mc.List("", "*", mailboxes)
+	}()
+
+	for m := range mailboxes {
+		fmt.Println(m.Name)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fetchUnseenMessages(mc *client.Client) []*imap.Message {
+	criteria := &imap.SearchCriteria{
+		WithoutFlags: []string{"\\Seen"},
+	}
+
+	set, err := mc.Search(criteria)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	messages := make(chan *imap.Message, 10)
+	done := make(chan error, 1)
+
+	seqset := new(imap.SeqSet)
+	seqset.AddNum(set...)
+
+	go func() {
+		done <- mc.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+	}()
+
+	res := make([]*imap.Message, 0)
+
+	for msg := range messages {
+		res = append(res, msg)
+	}
+
+	if err := <-done; err != nil {
+		log.Fatal(err)
+	}
+
+	return res
 }
 
--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module imail
 go 1.21.0
 
 require (
-	github.com/emersion/go-imap v1.2.1 // indirect
-	github.com/emersion/go-message v0.17.0 // indirect
+	github.com/emersion/go-imap v1.2.1
+	github.com/gookit/goutil v0.6.14
+	github.com/zalando/go-keyring v0.2.5
+	golang.org/x/term v0.13.0
+)
+
+require (
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/danieljoos/wincred v1.2.0 // indirect
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
-	github.com/emersion/go-textwrapper v0.0.0-20200911093747-65d896831594 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gookit/color v1.5.4 // indirect
-	github.com/gookit/goutil v0.6.14 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
